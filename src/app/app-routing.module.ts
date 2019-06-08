78782eb3d616d527d6818cd21ed93a2bc7bb4a31
thepsalmist <xf.xavierfrank@gmail.com>
2019-06-08 11:46:58 +0300

    configure routes for the application components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GitsearchComponent } from './gitsearch/gitsearch.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: "gitsearch", component: GitsearchComponent },
+  { path: "profile", component: ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, GitsearchComponent, ProfileComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { GitsearchComponent } from './gitsearch/gitsearch.component';
-import { ProfileComponent } from './profile/profile.component';
+// import { HomeComponent } from './home/home.component';
+// import { GitsearchComponent } from './gitsearch/gitsearch.component';
+// import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
-    GitsearchComponent,
-    ProfileComponent
+    routingComponents
+    // HomeComponent,
+    // GitsearchComponent,
+    // ProfileComponent
   ],
   imports: [
     BrowserModule,
